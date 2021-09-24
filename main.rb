fb2e4eba0e9a5efec545a913fee1fd556817fb37
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-09-24 12:24:46 +1000

    added display_list and module Inventory

--- main.rb
+++ main.rb
@@ -25,38 +25,46 @@ Problems statements- two(Sales report)
 5. margin
 =end
 
+module Inventory
 
-class Item
-  attr_accessor :name, :price
-  def initialize
-   @name = ["scented_candles", "greeting_cards", "wall_clocks", 
-    "photo_frames", "soft_toys", "perfume_bottles", "chocolate_boxes", "Jigsaw_puzzle_box", "souvenir_mugs"]
-    @price = [15, 10, 50, 18, 70, 150, 65, 78, 25]
-    # @item_sold_quantity = gets.strip.to_i
-  end
-  
-  #def update_quantity_candle= (value)
-  #end
+  # def Inventory.update_quantity
+  #   @item_sold_quantity = gets.strip.to_i
+  #   if @item_sold_quantity.instance_of? Integer
+       
+  #   end
 
-  def display_list
+  # end
+  class Item
+    attr_accessor :name, :price, :initial_quantity
+   def initialize
+     @name = ["scented_candles", "greeting_cards", "wall_clocks", 
+      "photo_frames", "soft_toys", "perfume_bottles", "chocolate_boxes", 
+      "Jigsaw_puzzle_box", "souvenir_mugs"]
+      @price = [15, 10, 50, 18, 70, 150, 65, 78, 25]
+      @initial_quantity = [65, 100, 20, 30, 20, 15, 25, 12, 20]
+   end
   end
+
+
+  def Inventory.display_list
+   item = Item.new
+   rows = []
+   i=0
+   while i < 9
+    rows << [item.name[i].capitalize, item.price[i], item.initial_quantity[i]]
+    rows << :separator 
+    i+=1
+   end
+
+   table = Terminal::Table.new :headings => ['Items', 'Price', "Quantity"], 
+   :rows => rows, :title => " Inventory Check "
+   return table
+  end
+
 end
+puts Inventory.display_list
 
-#list these two items
 
-candle=Item.new
-puts candle.name[0]
-puts candle.price[0]
-cards= Item.new
-puts cards.name[1]
-puts cards.price[1]
 
-rows = []
-rows << [candle.name[0], candle.price[0]]
-rows << :separator 
-rows << [cards.name[1], cards.price[1]]
 
-table = Terminal::Table.new :headings => ['Items', 'Price'], :rows => rows, 
-:title => " Inventory Check "
 
-puts table
